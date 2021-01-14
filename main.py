3b58b6b4c5123a327bb356a85e066e9541287e44
giftedWitness <aaron@niechzial.de>
2021-01-14 21:08:58 +0100

    Added clear command

--- main.py
+++ main.py
@@ -28,5 +28,8 @@ async def _8Ball(ctx, *, question):
     responses = ["Certainly!", "So shall be it.", "Without a doubt.", "Yes. For definite.", "You can rely on it.", "Probably", "Seems good to me.", "yes.", "meh, more like a yes though.", "Response hazy. Ask again.", "not rn, ask later.", "better not tell you now.", "Can't predict now", "Ok, FOCUS, and ask again.", "Don't count on it", "My reply is no.", "my sources say no.", "Doesn't look to good to me.", "more like a no..."]
     await ctx.send(f"Question: {question} \nAnswer: {random.choice(responses)}")
 
+@client.command()
+async def clear(ctx, amount = 5):
+    await ctx.channel.purge(limit = amount)
 
 client.run("Nzk5MzQzNjE0NDU0NjYxMTYw.YACMwA.DFOIdhF7Q3zIx00kIl9PS0n7ib0")
