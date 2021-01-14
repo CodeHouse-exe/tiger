94e65bbdeccc6d5bef9b65c36b17f649583215d6
giftedWitness <aaron@niechzial.de>
2021-01-14 22:51:06 +0100

    Added rickroll, quit and spam command

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import config
 import discord
 from discord.ext import commands
 import random
+import tokens
+import time
 
 client = commands.Bot(command_prefix = config.prefix)
 
@@ -23,13 +25,32 @@ if config.messageOnRemove == True:
 async def ping(ctx):
     await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
 
-@client.command(aliases = ["8ball"])
+@client.command(aliases = ["8ball", "8Ball"])
 async def _8Ball(ctx, *, question):
-    responses = ["Certainly!", "So shall be it.", "Without a doubt.", "Yes. For definite.", "You can rely on it.", "Probably", "Seems good to me.", "yes.", "meh, more like a yes though.", "Response hazy. Ask again.", "not rn, ask later.", "better not tell you now.", "Can't predict now", "Ok, FOCUS, and ask again.", "Don't count on it", "My reply is no.", "my sources say no.", "Doesn't look to good to me.", "more like a no..."]
+    responses = ["Certainly!", "Absolutely.", "Without a doubt.", "Yes. For definite.", "You can rely on it.", "Probably", "Seems good to me.", "yes.", "meh, more like a yes though.", "Response hazy. Ask again.", "not rn, ask later.", "better not tell you now.", "Can't predict now", "Ok, FOCUS, and ask again.", "Don't count on it", "My reply is no.", "my sources say no.", "Doesn't look too good to me.", "more like a no..."]
     await ctx.send(f"Question: {question} \nAnswer: {random.choice(responses)}")
+
+@client.command()
+async def spam(ctx, amount = 10):
+    time.sleep(2)
+    for index in range(0, amount):
+        await ctx.send(f"Spam")
+
+@client.command(aliases = ["surprise", "rr"])
+async def rickroll(ctx):
+    rr = open("tiger/rickroll.txt", "r")
+    await ctx.send(f"Here goes. \nReady?")
+    time.sleep(2)
+    for word in rr:
+        await ctx.send(word)
 
 @client.command()
 async def clear(ctx, amount = 5):
     await ctx.channel.purge(limit = amount)
 
-client.run("Nzk5MzQzNjE0NDU0NjYxMTYw.YACMwA.DFOIdhF7Q3zIx00kIl9PS0n7ib0")
+@client.command()
+async def quit(ctx):
+    await ctx.send("tiger is going offline.")
+    quit()
+
+client.run(tokens.token)
