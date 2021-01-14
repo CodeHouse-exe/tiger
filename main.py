87d593ae425c1db38d802f1dd6a991cbf54a6724
giftedWitness <aaron@niechzial.de>
2021-01-14 20:01:48 +0100

    Added member join and remove client events

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import discord
 from discord.ext import commands
+import config
 
 client = commands.Bot(command_prefix = "!")
 
 @client.event
 async def on_ready():
     print("Client ready.")
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} ' + config.onMemberJoin)
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} ' + config.onMemberRemove)
 
 client.run("Nzk5MzQzNjE0NDU0NjYxMTYw.YACMwA.DFOIdhF7Q3zIx00kIl9PS0n7ib0")
 
