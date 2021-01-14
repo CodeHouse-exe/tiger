aa77e4e63a6118ff1287580a1c02c5aa26a371b0
giftedWitness <aaron@niechzial.de>
2021-01-14 20:45:07 +0100

    Added ping and 8ball commands

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
-# CONFIG FILE
+# PREFIX
+prefix = "!"
 
+# CUSTOM MESSAGES
+messageOnJoin = True
+messageOnRemove = True
 onMemberJoin = "just joined the server."
 onMemberRemove = "just left the server."
+
+# CONTROL CENTER
+ping = True
+_8ball = True
--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+import config
 import discord
 from discord.ext import commands
-import config
+import random
 
-client = commands.Bot(command_prefix = "!")
+client = commands.Bot(command_prefix = config.prefix)
 
 @client.event
 async def on_ready():
     print("Client ready.")
 
-@client.event
-async def on_member_join(member):
-    print(f'{member} ' + config.onMemberJoin)
+if config.messageOnJoin == True:
+    @client.event
+    async def on_member_join(member):
+        print(f'{member} ' + config.onMemberJoin)
 
-@client.event
-async def on_member_remove(member):
-    print(f'{member} ' + config.onMemberRemove)
+if config.messageOnRemove == True:
+    @client.event
+    async def on_member_remove(member):
+        print(f'{member} ' + config.onMemberRemove)
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+
+@client.command(aliases = ["8ball"])
+async def _8Ball(ctx, *, question):
+    responses = ["Certainly!", "So shall be it.", "Without a doubt.", "Yes. For definite.", "You can rely on it.", "Probably", "Seems good to me.", "yes.", "meh, more like a yes though.", "Response hazy. Ask again.", "not rn, ask later.", "better not tell you now.", "Can't predict now", "Ok, FOCUS, and ask again.", "Don't count on it", "My reply is no.", "my sources say no.", "Doesn't look to good to me.", "more like a no..."]
+    await ctx.send(f"Question: {question} \nAnswer: {random.choice(responses)}")
+
 
 client.run("Nzk5MzQzNjE0NDU0NjYxMTYw.YACMwA.DFOIdhF7Q3zIx00kIl9PS0n7ib0")
-
