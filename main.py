26ba5a9e2595b96ea5bed35bf46afa5ebfd9214a
giftedWitness <aaron@niechzial.de>
2021-01-15 14:16:03 +0100

    Added mention for ban reason output

--- main.py
+++ main.py
@@ -52,10 +52,10 @@ async def kick(ctx, member : discord.Member, *, reason = None):
 async def ban(ctx, member : discord.Member, *, reason = None):
     if reason == None:
         await member.ban(reason = reason)
-        await ctx.send(f"Banned {member.name}#{member.discriminator} for no reason, apparently.")
+        await ctx.send(f"Banned {member.mention} for no reason, apparently.")
     elif reason != None:
         await member.ban(reason = reason)
-        await ctx.send(f"Banned {member.name}#{member.discriminator} for {reason}")
+        await ctx.send(f"Banned {member.mention} for {reason}")
 
 @client.command()
 async def unban(ctx, *, member):
