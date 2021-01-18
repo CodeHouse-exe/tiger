55e83dbc8194f44096895f15e9442a91fcd969e2
giftedWitness <aaron@niechzial.de>
2021-01-18 10:51:25 +0100

    Added customisable 8Ball responses

--- config.py
+++ config.py
@@ -5,11 +5,12 @@ prefix = "!"
 onMemberJoin = "just joined the server."
 onMemberRemove = "just left the server."
 status = ["you like he did...", "Space Invaders", "tag with Donald Trump", "absolutely nothing", ]
+_8BallResponses = ["Certainly!", "Absolutely.", "Without a doubt.", "Yes. For definite.", "You can rely on it.", "Probably", "Seems good to me.", "yes.", "meh, more like a yes though.", "Response hazy. Ask again.", "not rn, ask later.", "better not tell you now.", "Can't predict now", "Ok, FOCUS, and ask again.", "Don't count on it", "My reply is no.", "my sources say no.", "Doesn't look too good to me.", "more like a no..."]
 
 # CONTROL CENTER
 messageOnJoin = True
 messageOnRemove = True
 ping = True
-_8ball = True
+_8Ball = True
 customStatus = True
 statusInterval = 10 # seconds
--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def ping(ctx):
 
 @client.command(aliases = ["8ball", "8Ball"])
 async def _8Ball(ctx, *, question):
-    responses = ["Certainly!", "Absolutely.", "Without a doubt.", "Yes. For definite.", "You can rely on it.", "Probably", "Seems good to me.", "yes.", "meh, more like a yes though.", "Response hazy. Ask again.", "not rn, ask later.", "better not tell you now.", "Can't predict now", "Ok, FOCUS, and ask again.", "Don't count on it", "My reply is no.", "my sources say no.", "Doesn't look too good to me.", "more like a no..."]
+    responses = config._8BallResponses
     await ctx.send(f"Question: {question} \nAnswer: {random.choice(responses)}")
 
 @client.command()
