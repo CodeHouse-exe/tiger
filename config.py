141452649601571f1403180000155802bc7def76
giftedWitness <aaron@niechzial.de>
2021-01-22 23:01:55 +0100

    Added config.json

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ prefix = "!"
 # CUSTOMISE
 onMemberJoin = "just joined the server."
 onMemberRemove = "just left the server."
-status = ["you like he did...", "Space Invaders", "tag with Donald Trump", "absolutely nothing", ]
+status = ["you like he did...", "Space Invaders", "tag with Donald Trump", "absolutely nothing"]
 _8BallResponses = ["Certainly!", "Absolutely.", "Without a doubt.", "Yes. For definite.", "You can rely on it.", "Probably", "Seems good to me.", "yes.", "meh, more like a yes though.", "Response hazy. Ask again.", "not rn, ask later.", "better not tell you now.", "Can't predict now", "Ok, FOCUS, and ask again.", "Don't count on it", "My reply is no.", "my sources say no.", "Doesn't look too good to me.", "more like a no..."]
 
 # CONTROL CENTER
