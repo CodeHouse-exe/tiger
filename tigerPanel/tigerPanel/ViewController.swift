881a788e3784e44ad556812721fda1d52ee79491
Aaron Niechzial <aaron@niechzial.de>
2021-01-23 14:58:51 +0100

    Initial Commit

---
+++ tigerPanel/tigerPanel/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  tigerPanel
+//
+//  Created by Aaron on 23.01.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
