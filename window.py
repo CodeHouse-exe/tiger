4ef75eb1310b62c6f4173fb1a01d35bc28745369
Aaron Niechzial <aaron@niechzial.de>
2021-01-23 15:39:02 +0100

    Create window.py

---
+++ window.py
@@ -0,0 +1,26 @@
+import tkinter
+import json
+
+w = tkinter.Tk()
+w.geometry("600x600")
+w.title("tigerPanel (Universal) v0.0.1")
+
+def nameInp():
+    print("Hi")
+
+messageOnJoinCheckbutton = tkinter.Checkbutton(w, text="Print a message to console when a player joins", onvalue=True, offvalue=False)
+messageOnJoinCheckbutton.place(x=1, y=1)
+messageOnRemoveCheckbutton = tkinter.Checkbutton(w, text="Print a message to console when a player is removed", onvalue=True, offvalue=False)
+messageOnRemoveCheckbutton.place(x=1, y=30)
+commandPingCheckbutton = tkinter.Checkbutton(w, text="Enable ping-command", onvalue=True, offvalue=False)
+commandPingCheckbutton.place(x=1, y=60)
+command8BallCheckbutton = tkinter.Checkbutton(w, text="Enable 8Ball-command", onvalue=True, offvalue=False)
+command8BallCheckbutton.place(x=1, y=90)
+customStatusCheckbutton = tkinter.Checkbutton(w, text="Enable custom status", onvalue=True, offvalue=False)
+customStatusCheckbutton.place(x=1, y=120)
+customStatusLabel = tkinter.Label(w, text="Status Interval:")
+customStatusLabel.place(x=1, y=180)
+customStatusInterval = tkinter.Entry(w)
+customStatusInterval.place(x=100, y=180)
+
+w.mainloop()
