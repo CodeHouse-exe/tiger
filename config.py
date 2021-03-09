4785485de76773810c6b80538d9d63358027a18c
CodeHouse-exe <aaron@niechzial.de>
2021-03-09 11:02:51 +0100

    Fixed indentation

--- config.py
+++ config.py
@@ -20,10 +20,10 @@ for key in data["token"]:
 
 status = ["you like he did...", "Space Invaders", "tag with Donald Trump", "absolutely nothing"]
 _8_ball_responses = {"Certainly!", "Absolutely.", "Without a doubt.", "Yes. For definite.", "You can rely on it.",
-                     "Probably", "Seems good to me.", "yes.", "meh, more like a yes though.",
-                     "Response hazy. Ask again.", "not rn, ask later.", "better not tell you now.", "Can't predict now",
-                     "Ok, FOCUS, and ask again.", "Don't count on it", "My reply is no.", "my sources say no.",
-                     "Doesn't look too good to me.", "more like a no..."}
+                    "Probably", "Seems good to me.", "yes.", "meh, more like a yes though.",
+                    "Response hazy. Ask again.", "not rn, ask later.", "better not tell you now.", "Can't predict now",
+                    "Ok, FOCUS, and ask again.", "Don't count on it", "My reply is no.", "my sources say no.",
+                    "Doesn't look too good to me.", "more like a no..."}
 
 # ERROR MESSAGES
 clear_missing_arg = "So, uh, how many messages do you want me to delete?"
